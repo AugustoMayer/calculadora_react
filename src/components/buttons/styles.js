7513d1ebf270476ddce84dcec6c94ebadaaf86e0
Augusto Mayer <150570739+AugustoMayer@users.noreply.github.com>
2024-07-02 22:27:01 -0300

    Add files via upload

---
+++ src/components/buttons/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+export const ButtonContainer = styled.button`
+    margin: 0 0 0 0;
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAF0;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    width: 25%;
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
---
+++ src/components/input/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items:center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    padding: 0 0 2px 0;
+
+    input{
+        width: 100%;
+        height: 90px;
+        background-color: #AAAAFF;
+        border: 0;
+        color: #FFF;
+        padding: 0 10px;
+        font-size: 48px;
+        font-family: 'Roboto';
+    }
+`
